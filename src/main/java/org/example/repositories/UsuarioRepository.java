8c39b1175233702d1a0412e9fb0bde0e06b04072
RafaelRodrigues666 <patriciaoliveira.po270@gmail.com>
2025-06-09 17:09:21 -0300

    UserRepository

--- src/main/java/org/example/repositories/UsuarioRepository.java
+++ src/main/java/org/example/repositories/UsuarioRepository.java
@@ -1,4 +1,10 @@
 package org.example.repositories;
 
-public interface UsuarioRepository {
+import org.example.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
 }
