1d8722be8fb492b9ba684478e59a885fdf625951
RafaelRodrigues666 <patriciaoliveira.po270@gmail.com>
2025-06-14 10:24:03 -0300

    SupplierController SupplierService
    Signed-off-by: gustavorodriguessts <gustavordrigues848@gmail.com>

--- src/main/java/org/example/controller/FornecedorController.java
+++ src/main/java/org/example/controller/FornecedorController.java
@@ -1,4 +1,51 @@
 package org.example.controller;
 
+import org.example.entities.Fornecedor;
+import org.example.exceptions.ResourceNotFoundException;
+import org.example.services.FornecedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fornecedores")
 public class FornecedorController {
+
+    @Autowired
+    private FornecedorService fornecedorService;
+
+    @GetMapping
+    public List<Fornecedor> listarTodos() {
+        return fornecedorService.listarTodos();
+    }
+
+    @PostMapping
+    public Fornecedor criar(@RequestBody Fornecedor fornecedor) {
+        return fornecedorService.salvar(fornecedor);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Fornecedor> buscarPorId(@PathVariable("id") Long id) {
+        return fornecedorService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Fornecedor> atualizar(@PathVariable("id") Long id, @RequestBody Fornecedor fornecedor) {
+        try {
+            Fornecedor atualizar = fornecedorService.atualizar(id, fornecedor);
+            return ResponseEntity.ok(atualizar);
+        } catch (ResourceNotFoundException e) {
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+        fornecedorService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/org/example/services/FornecedorService.java
+++ src/main/java/org/example/services/FornecedorService.java
@@ -1,36 +1,45 @@
 package org.example.services;
 
 import org.example.entities.Fornecedor;
+import org.example.exceptions.ResourceNotFoundException;
 import org.example.repositories.FornecedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FornecedorService {
 
-    private final FornecedorRepository fornecedorRepository;
-
     @Autowired
-    public FornecedorService(FornecedorRepository fornecedorRepository) {
-        this.fornecedorRepository = fornecedorRepository;
-    }
+    private FornecedorRepository fornecedorRepository;
 
     public List<Fornecedor> listarTodos() {
         return fornecedorRepository.findAll();
     }
 
-    public Fornecedor buscarPorId(Long id) {
-        return fornecedorRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Fornecedor não Encontrado"));
+    public Optional<Fornecedor> buscarPorId(Long id) {
+        return fornecedorRepository.findById(id);
     }
 
     public Fornecedor salvar(Fornecedor fornecedor) {
         return fornecedorRepository.save(fornecedor);
     }
 
+    public Fornecedor atualizar(Long id, Fornecedor fornecedorDetails) {
+        Fornecedor fornecedor = fornecedorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Fornecedor não encontrado com id " + id));
+
+        fornecedor.setNomeFornecedor(fornecedorDetails.getNomeFornecedor());
+        fornecedor.setCnpj(fornecedorDetails.getCnpj());
+        fornecedor.setTelefone(fornecedorDetails.getTelefone());
+
+        return fornecedorRepository.save(fornecedor);
+    }
+
     public void deletar(Long id) {
-        fornecedorRepository.deleteById(id);
+        Fornecedor fornecedor = fornecedorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Fornecedor não encontrado com id " + id));
     }
 }
