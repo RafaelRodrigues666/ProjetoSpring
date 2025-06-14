d39cd9dfc73073e1ed56bf6054a61c6b5cbc30ae
RafaelRodrigues666 <patriciaoliveira.po270@gmail.com>
2025-06-14 10:43:41 -0300

    UserController UserService

--- src/main/java/org/example/controller/UsuarioController.java
+++ src/main/java/org/example/controller/UsuarioController.java
@@ -1,4 +1,51 @@
 package org.example.controller;
 
+import org.example.entities.Usuario;
+import org.example.exceptions.ResourceNotFoundException;
+import org.example.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> listarTodos() {
+        return usuarioService.listarTodos();
+    }
+
+    @PostMapping
+    public Usuario criar(@RequestBody Usuario usuario) {
+        return usuarioService.salvar(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable("id") Long id) {
+        return usuarioService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizar(@PathVariable("id") Long id, @RequestBody Usuario usuario) {
+        try {
+            Usuario atualizar = usuarioService.atualizar(id, usuario);
+            return ResponseEntity.ok(atualizar);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+        usuarioService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/org/example/services/UsuarioService.java
+++ src/main/java/org/example/services/UsuarioService.java
@@ -1,36 +1,46 @@
 package org.example.services;
 
 import org.example.entities.Usuario;
+import org.example.exceptions.ResourceNotFoundException;
 import org.example.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
 
-    private final UsuarioRepository usuarioRepository;
-
     @Autowired
-    public UsuarioService(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
-    }
+    private UsuarioRepository usuarioRepository;
 
     public List<Usuario> listarTodos() {
         return usuarioRepository.findAll();
     }
 
-    public Usuario buscarPorId(Long id) {
-        return usuarioRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuário não Encontrado"));
+    public Optional<Usuario> buscarPorId(Long id) {
+        return usuarioRepository.findById(id);
     }
 
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 
+    public Usuario atualizar(Long id, Usuario usuarioDetails) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado com id " + id));
+
+        usuario.setNome(usuarioDetails.getNome());
+        usuario.setEmail(usuarioDetails.getEmail());
+        usuario.setSenha(usuarioDetails.getSenha());
+
+        return usuarioRepository.save(usuario);
+    }
+
     public void deletar(Long id) {
-        usuarioRepository.deleteById(id);
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado com id " + id));
+        usuarioRepository.delete(usuario);
     }
 }
