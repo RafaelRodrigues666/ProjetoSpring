939ae0419c199cefa6ad62eb76a6ea18c93f638d
gustavorodriguessts <gustavordrigues848@gmail.com>
2025-06-14 10:08:46 -0300

    PaymentMethodController

--- src/main/java/org/example/controller/FormaPagamentoController.java
+++ src/main/java/org/example/controller/FormaPagamentoController.java
@@ -1,4 +1,53 @@
 package org.example.controller;
 
+
+import jakarta.persistence.Id;
+import org.example.entities.FormaPagamento;
+import org.example.exceptions.ResourceNotFoundException;
+import org.example.services.FormaPagamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/forma-pagamentos")
 public class FormaPagamentoController {
+
+    @Autowired
+    private FormaPagamentoService formaPagamentoService;
+
+    @GetMapping
+    public List<FormaPagamento> listarTodos() {
+        return formaPagamentoService.listarTodos();
+    }
+
+    @PostMapping("/{id}")
+    public FormaPagamento criar(@RequestBody FormaPagamento formaPagamento) {
+        return formaPagamentoService.salvar(formaPagamento);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FormaPagamento> buscarPorId(@PathVariable("id") Long id) {
+        return formaPagamentoService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FormaPagamento> atualizar(@PathVariable("id") Long id, @RequestBody FormaPagamento formaPagamento) {
+        try {
+            FormaPagamento atualizar = formaPagamentoService.atualizar(id, formaPagamento);
+            return ResponseEntity.ok(atualizar);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+        formaPagamentoService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
